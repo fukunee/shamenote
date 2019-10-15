41e217e233ca123c651498e1bb446308aa46d4f4
lwc <1327987604@qq.com>
2019-10-15 16:22:32 +0800

    Modify lc_1042 solution heavily

--- leetCode/lwc/lc_1042.java
+++ leetCode/lwc/lc_1042.java
@@ -1,59 +1,44 @@
 package solution2;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author lwc
  * @date 2019/10/14 17:10
  */
 public class U1042 {
+
     public int[] gardenNoAdj(int N, int[][] paths) {
-        int[] values = new int[N];
-        Map<Integer, Map> map = new HashMap<>();
-
-        Arrays.sort(paths, (v1, v2) -> {
-            if(v1[0] > v2[0]) return 1;
-            else if(v1[0] == v2[0]) return Integer.compare(v1[1],v2[1]);
-            else return -1;
-        });
-
+        int[] v = new int[N];
+        int[][] visit = new int[N][5];
+        List<Integer>[] array = new List[N];
+        for (int i = 0; i < N; i++)
+            array[i] = new ArrayList();
         for (int[] path : paths) {
-            int x = path[0] - 1;
-            int y = path[1] - 1;
-            if (x > y) {
-                int t = x;
-                x = y;
-                y = t;
-            }
-            if (values[x] == 0) values[x] = 1;
-
-            int k = 1;
-            while (k < values[y] || k == values[x] || map.containsKey(y) && map.get(y).containsKey(k))
-                k++;
-            values[y] = k;
-            if (map.containsKey(y)) map.get(y).put(values[x], 1);
-            else {
-                Map<Integer, Integer> m = new HashMap<>();
-                m.put(values[x], 1);
-                map.put(y, m);
-            }
+            int x = Math.min(path[0], path[1]) - 1;
+            int y = Math.max(path[0], path[1]) - 1;
+            array[x].add(y);
         }
         for (int i = 0; i < N; i++) {
-            if (values[i] == 0) {
-                int k = 1;
-                while (map.containsKey(i) && map.get(i).containsKey(k)) k++;
-                values[i] = k;
+            if (v[i] == 0) v[i] = 1;
+            int min = 1;
+            while (min == v[i]) min++;
+            for (int value : array[i]) {
+                if (v[value] == 0) v[value] = min;
+                if (v[value] == v[i]) {
+                    int k = v[value]+1;
+                    while (k == v[i] || visit[value][k] != 0) k++;
+                    v[value] = k;
+                }
+                visit[value][v[i]] = 1;
             }
         }
-        return values;
+        return v;
     }
 
     public static void main(String[] args) {
-        new U1042().gardenNoAdj(5,new int[][]{{3,4},{4,2},{3,2},{1,3}});
-        //new U1042().gardenNoAdj(8,new int[][]{{7,4},{3,7},{1,5},{5,4},{7,1},{3,1},{4,3},{6,5}});
-       // new U1042().gardenNoAdj(10,new int[][]{{10,8},{2,1},{4,5},{10,7},{4,2},{7,6},{8,1},{8,4},{7,5},{10,2},{3,5},{3,6}});
+        new U1042().gardenNoAdj(3,new int[][]{{3,1},{1,2},{2,3}});
+        new U1042().gardenNoAdj(7,new int[][]{{7,4},{3,7},{1,5},{5,4},{7,1},{3,1},{4,3},{6,5}});
     }
 
 }
