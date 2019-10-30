f10793c87d560cd52e77f9f2db8c47b61d311651
Yllnss <806970403@163.com>
2019-10-30 10:51:27 +0800

    Add Yllnss leetcode solutions slightly

---
+++ leetCode/ljz/leetcode/leetcode/editor/cn/DeepDarkFraction.java
@@ -0,0 +1,80 @@
+//有一个同学在学习分式。他需要将一个连分数化成最简分数，你能帮助他吗？
+//
+// 
+//
+// 连分数是形如上图的分式。在本题中，所有系数都是大于等于0的整数。 
+//
+// 
+//
+// 输入的cont代表连分数的系数（cont[0]代表上图的a0，以此类推）。返回一个长度为2的数组[n, m]，使得连分数的值等于n / m，且n, m最大公约数为1。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：cont = [3, 2, 0, 2]
+//输出：[13, 4]
+//解释：原连分数等价于3 + (1 / (2 + (1 / (0 + 1 / 2))))。注意[26, 8], [-13, -4]都不是正确答案。 
+//
+// 示例 2： 
+//
+// 输入：cont = [0, 0, 3]
+//输出：[3, 1]
+//解释：如果答案是整数，令分母为1即可。 
+//
+// 限制： 
+//
+// 
+// cont[i] >= 0 
+// 1 <= cont的长度 <= 10 
+// cont最后一个元素不等于0 
+// 答案的n, m的取值都能被32位int整型存下（即不超过2 ^ 31 - 1）。 
+// 
+//
+
+package cn;
+
+public class DeepDarkFraction {
+    public static void main(String[] args) {
+
+        Solution solution = new DeepDarkFraction().new Solution();
+        int[] a = {3, 2, 0, 2};
+        solution.fraction(a);
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] fraction(int[] cont) {
+            int[] ret = new int[2];
+            if (cont.length == 1) {
+                ret[0] = cont[0];
+                ret[1] = 1;
+                return ret;
+            }
+            int i = cont.length - 2, up = 1, down = cont[i + 1];
+            for (; i >= 0; i--) {
+                up += cont[i] * down;
+                ret = reduce1(up, down);
+                up = ret[1];
+                down = ret[0];
+            }
+            return ret;
+        }
+
+        private int[] reduce1(int a, int b) {
+            int[] ret = new int[2];
+            int rem = 0, n = a, m = b;
+            while (m != 0) {
+                rem= n % m;
+                n = m;
+                m = rem;
+            }
+            ret[0] = a / n;
+            ret[1] = b / n;
+            return ret;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
