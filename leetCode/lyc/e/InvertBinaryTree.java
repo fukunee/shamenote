d02469b683188bae6b2ba40bf77f7df834f61cc7
a-yuyuyu <55120797+a-yuyuyu@users.noreply.github.com>
2019-11-27 11:44:45 +0800

    Add my leetcode solutions slightly

---
+++ leetCode/lyc/e/InvertBinaryTree.java
@@ -0,0 +1,39 @@
+package easy;
+/*226*/
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InvertBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+  }
+    public static void main(String[] args){
+
+    }
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if(root == null){
+                return null;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while(!queue.isEmpty()){
+                TreeNode node = queue.poll();
+                TreeNode temp = node.left;
+                node.left = node.right;
+                node.right = temp;
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            return root;
+        }
+    }
+}
